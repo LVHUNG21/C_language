74ea1b5b77c7119a0ce86b3ca652accc0211c06e
LeVanHung34554 <hung0911248587@gmail.com>
2023-08-31 12:57:18 +0700

    update

---
+++ variable.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a = 438;
+    int b = 2;
+    int c = 2.2;
+    char character = 'h';
+    printf("a-b=%d", a - b);
+    printf("a+b=%d\n", a + b);
+    printf("a*b=%d\n", a * b);
+    // to display a real number on the screen, us %f
+    // printf("a=%f\n", c);
+    printf("a=%d\n", c);
+    printf("a/b=%d\n", a / b);
+    // to print character , use %c format;
+    printf("character=%c\n", character);
+    printf("a = %d\n", 'a');
+    printf("b = %d\n", 'b');
+    printf("c = %d\n", 'c');
+    long long aa = 384847522;
+    long long bb = 988347273;
+    printf("%lld\n", aa * bb);
+    return 0;
+    return 0;
+}
