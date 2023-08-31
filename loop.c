9d3d3e4a68a89b67099f2020da5e60438853ef5c
Sinh Vien TLU ( Pass:123456 ) <hung0911248587@gmail.com>
2023-08-31 17:26:52 +0700

    update

--- loop.c
+++ loop.c
@@ -1,20 +1,51 @@
-#include<stdio.h>
-int main(){
-    for(int i=10;i>0;i--){
-        printf("%d\n",i);
-    }   
-    int a,b;
-    a=2;b=6;
+#include <stdio.h>
+int main()
+{
+    for (int i = 10; i > 0; i--)
+    {
+        printf("%d\n", i);
+    }
+    int a, b;
+    a = 2;
+    b = 6;
 
     int sum;
-    for(int i=a;i<b;i++){
-       sum=sum+i; 
+    for (int i = a; i < b; i++)
+    {
+        sum = sum + i;
     }
-    printf("%d",sum);
-    for(int i=0;i<=b;i++){
-        if(i%2==1){
-            sum+=1;
+    printf("%d", sum);
+    for (int i = 0; i <= b; i++)
+    {
+        if (i % 2 == 1)
+        {
+            sum += 1;
         }
     }
+    int aw = 1;
+    while (b > 0)
+    {
+        aw *= a;
+        b--;
+    }
+    while (a <= b)
+    {
+        if (a % 3 == 0 && a % 5 == 0)
+        {
+            printf("%d", a);
+        }
+        a++;
+    }
+    int i = 1;
+    do
+    {
+        printf("do-while");
+    } while (i < 1);
+
+    while (i < 1)
+    {
+        printf("while");
+    }
+
     return 0;
 }
